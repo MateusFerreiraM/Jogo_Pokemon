6391d809f96bbed6be14280055fd70d10911ba6e
B-Cut <cgoncalves@id.uff.br>
2022-11-12 17:28:42 -0300

    Movimentos implementado

--- src/Movimentos.java
+++ src/Movimentos.java
@@ -1,3 +1,23 @@
 public class Movimentos {
-    
+    static enum Categoria{ //Esse enum diz respeito somente aos movimentos
+        FISICO,
+        ESPECIAL,
+        STATUS
+    }
+
+    private static int movesCriados= 0; //O id de um movimento se baseia em quando ele foi criado
+    public int id;
+    public Tipos tipo;
+    public int forca;
+    public Categoria categoria;
+    public int precisao;
+
+    public Movimentos(Tipos tipo, int forca, Categoria categoria, int precisao){
+        this.id = movesCriados;
+        this.forca = forca;
+        this.categoria = categoria;
+        this.precisao = precisao;
+        
+        movesCriados++;
+    }
 }
