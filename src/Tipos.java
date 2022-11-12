31421ae47fa6b158e387424684d53a8b1deddd01
B-Cut <cgoncalves@id.uff.br>
2022-11-12 17:10:11 -0300

    Renomeado classe main, classes basicas do modelo adicionadas

---
+++ src/Tipos.java
@@ -0,0 +1,19 @@
+public enum Tipos {
+    NORMAL,
+    FOGO,
+    AGUA,
+    GRAMA,
+    LUTADOR,
+    VENENO,
+    ELETRICO,
+    TERRA,
+    PEDRA,
+    PSIQUICO,
+    INSETO,
+    GELO,
+    FANTASMA,
+    FERRO,
+    DRAGAO,
+    SOMBRIO,
+    FADA
+}
