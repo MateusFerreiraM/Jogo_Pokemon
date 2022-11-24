b14d04b52e4fefa771d7c783f172dd1cdf9eaf42
IN-CaelG <cael.santos@injunior.com.br>
2022-11-24 18:29:21 -0300

    Ajustes a classe treinador

--- src/Treinador.java
+++ src/Treinador.java
@@ -1,17 +1,17 @@
-import java.util.List;
+import java.util.ArrayList;
 
 public class Treinador extends Identificacao {
-    private static int treinadoresCriados = 0;
-    public String regiao;
+    private static int TotalTreinadores = 0;
+    private String regiao;
     public int qtdPokemon;
-    public List<Pokemon> time;
+    public ArrayList<Pokemon> time;
 
     public Treinador(String nome, String regiao) {
         this.nome = nome;
         this.regiao = regiao;
-        this.id = treinadoresCriados;
-        treinadoresCriados++;
-        // TODO: times
+        this.id = TotalTreinadores;
+        this.time = new ArrayList<Pokemon>();
+        TotalTreinadores++;
     }
 
     public int getQtdPokemon() {
@@ -22,7 +22,15 @@ public class Treinador extends Identificacao {
         return regiao;
     }
 
-    public List<Pokemon> getTime() {
+    public ArrayList<Pokemon> getTime() {
         return time;
+    }
+
+    public static int getTotalTreinadores(){
+        return TotalTreinadores;
+    }
+
+    public void addPokemon(Pokemon pokemon){
+        this.time.add(pokemon);
     }
 }
