a3c140a479945bc3fb5a47bfa2113de56443702b
MateusFerreiraMachado <111912705+MateusFerreiraMachado@users.noreply.github.com>
2022-12-06 15:46:16 -0300

    Update CadastroTreinador.java
    Rascunho de como adicionar no arquivo

--- src/main/java/uff/tank/seraphine/CadastroTreinador.java
+++ src/main/java/uff/tank/seraphine/CadastroTreinador.java
@@ -1,6 +1,26 @@
 package uff.tank.seraphine;
 
-//import org.json.JSONException;
-//import org.json.JSONObject;
+import org.json.simple.JSONObject;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.*;
 
-//const obj =
+/*public class CadastroTreinador(String nome, String regiao){
+    FileWriter writeFile = null;
+    JSONObject obetoJson = new JSONObject();
+
+    obetoJson.put("nome", "Mateus");
+    obetoJson.put("sobrenome", "Ferreira");
+    obetoJson.put("idade", "22");
+    obetoJson.put("time", "VASCO");
+
+    try {
+        writeFile = new FileWriter("assets/dados.json");
+        writeFile.write(obetoJson.toJSONString());
+        writeFile.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+    System.out.println(obetoJson.toJSONString());
+}*/
