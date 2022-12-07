26c18515bf20e79af9e1ff28eec97ec84444dcec
MateusFerreiraMachado <111912705+MateusFerreiraMachado@users.noreply.github.com>
2022-12-07 12:30:35 -0300

    Leitura e Escrita no arquivo JSON
    Ainda falta fazer as exceções

--- src/main/java/uff/tank/seraphine/CadastroTreinador.java
+++ src/main/java/uff/tank/seraphine/CadastroTreinador.java
@@ -1,26 +1,64 @@
 package uff.tank.seraphine;
 
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.logging.Logger;
+
 import org.json.*;
 
-/*public class CadastroTreinador(String nome, String regiao){
-    FileWriter writeFile = null;
-    JSONObject obetoJson = new JSONObject();
 
-    obetoJson.put("nome", "Mateus");
-    obetoJson.put("sobrenome", "Ferreira");
-    obetoJson.put("idade", "22");
-    obetoJson.put("time", "VASCO");
+public class CadastroTreinador{
+    public static void main(String[] args) {
+        /*
+        // Salvar treinador no arquivo
+        FileWriter writeFile = null;
+        JSONObject obetoJson = new JSONObject();
+    
+        obetoJson.put("id", "01");
+        obetoJson.put("nome", "Mateus");
+        obetoJson.put("regiao", "Guadalupe");
+    
+        try {
+            writeFile = new FileWriter("assets/dados.json");
+            writeFile.write(obetoJson.toJSONString());
+            writeFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    
+        System.out.println(obetoJson.toJSONString());
+         */
+        
+        // Ler treinador do arquivo
+         /* 
+        JSONObject obetoJson;
+        JSONParser parser = new JSONParser();
 
-    try {
-        writeFile = new FileWriter("assets/dados.json");
-        writeFile.write(obetoJson.toJSONString());
-        writeFile.close();
-    } catch (IOException e) {
-        e.printStackTrace();
+        Treinador treinador = new Treinador();
+
+        try{
+
+            obetoJson = (JSONObject) parser.parse(new FileReader("assets/dados.json"));
+
+            treinador.setNome((String) obetoJson.get("nome"));
+            treinador.setId((String) obetoJson.get("id"));
+            treinador.setRegiao((String) obetoJson.get("regiao"));
+
+            System.out.println("Treinador criado = " + treinador.toString());
+
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }catch (ParseException e) {
+            e.printStackTrace();
+        }
+        */
     }
-
-    System.out.println(obetoJson.toJSONString());
-}*/
+}
