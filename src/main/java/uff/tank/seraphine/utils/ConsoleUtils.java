4322c72c5427bfb85a58346e020521723f251a19
CaelG <cgoncalves@id.uff.br>
2022-12-06 23:04:47 -0300

    Limpeza do console agora funciona corretamente

--- src/main/java/uff/tank/seraphine/utils/ConsoleUtils.java
+++ src/main/java/uff/tank/seraphine/utils/ConsoleUtils.java
@@ -1,7 +1,16 @@
 package uff.tank.seraphine.utils;
 
+
 public class ConsoleUtils {
     public static void clearConsole(){
-        System.out.println("\033[H\033[2J");//Sequencia de escape para reposicionar o cursor e limpar o terminal
+        //System.out.println("\033[H\033[2J");//Sequencia de escape para reposicionar o cursor e limpar o terminal
+        try{
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
