d979e0f60b0bb96be3bc4abb7386da1f85cec79c
MateusFerreiraMachado <111912705+MateusFerreiraMachado@users.noreply.github.com>
2022-12-07 15:26:13 -0300

    Update TelaSelecionaTreinador.java

--- src/main/java/uff/tank/seraphine/telas/TelaSelecionaTreinador.java
+++ src/main/java/uff/tank/seraphine/telas/TelaSelecionaTreinador.java
@@ -1,14 +1,10 @@
 package uff.tank.seraphine.telas;
-
-import uff.tank.seraphine.telas.Tela;
-import uff.tank.seraphine.telas.TelaContext;
-
 import java.util.Scanner;
 
 public class TelaSelecionaTreinador extends Tela {
     @Override
     public void mostrarTela(){
-        System.out.println("=====Selecionar Treinador=====");
+        System.out.println("===== Selecionar Treinador =====");
         //TODO: Exibir lista treinadores
         System.out.println("X- Sair");
     }
