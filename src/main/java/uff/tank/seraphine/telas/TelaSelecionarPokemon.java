30d2253f292a93fedd85a9de2f454d93b17fb353
CaelG <cgoncalves@id.uff.br>
2022-12-10 19:45:57 -0300

    Seleção de pokemon implementada

--- src/main/java/uff/tank/seraphine/telas/TelaSelecionarPokemon.java
+++ src/main/java/uff/tank/seraphine/telas/TelaSelecionarPokemon.java
@@ -2,31 +2,71 @@ package uff.tank.seraphine.telas;
 
 import org.json.simple.JSONObject;
 
+import uff.tank.seraphine.Pokemon;
+import uff.tank.seraphine.Treinador;
+import uff.tank.seraphine.utils.JSONUtils;
 import uff.tank.seraphine.verPokedex;
+
+import java.awt.*;
 
 public class TelaSelecionarPokemon extends Tela {
     @Override
     public void mostrarTela() {
         System.out.println("---------- Selecionar Pokemon ----------\n");
-        JSONObject obj = verPokedex.mostrarPokedex();
-        System.out.println("Lista de Pokemons : " + obj.get("pokemons"));
+        if( this.contexto.getTreinador().pokemonAtual != null){
+            System.out.println("Pokémon atual: " + this.contexto.getTreinador().pokemonAtual.getNome());
+        } else {
+            System.out.println("Você não possuí nenhum Pokémon selecionado!");
+        }
+
+        System.out.println("Lista de Pokemons : " );
+        for(Pokemon pkmn: this.contexto.getTreinador().getPokemons()){
+            System.out.println(pkmn.getId() + " - " + pkmn.getNome() + "/" + pkmn.getTipo());
+        }
         System.out.println("\nV - Voltar ao menu principal");
         System.out.println("X - Sair");
+        System.out.print(">");
 
         String escolha = this.contexto.getUserInput();
 
-        switch (escolha) {
-            case "v":
-            case "V":
-                this.trocarTela(new TelaMenuPrincipal(this.contexto));
-                break;
-            case "x":
-            case "X":
-                this.contexto.sairPrograma();
-                break;
-            default:
-                System.out.println("Por favor insira um valor válido");
-                break;
+        boolean isNumber = false;
+
+
+        try {
+            //Tenta ver se o input é um número que pode ser Id
+            //Se o parse falhar, a string contém letras, logo, não é um Id
+            Integer.parseInt(escolha);
+            isNumber = true;
+        } catch (NumberFormatException e){
+            isNumber = false;
+        }
+        if(isNumber){
+            boolean naLista = false;
+            for(Pokemon i : this.contexto.getTreinador().getPokemons()){
+                if(i.getId() == Integer.parseInt(escolha)){
+                    this.contexto.getTreinador().setPokemonAtual(i);
+                    naLista = true;
+                    break;
+                }
+            }
+
+            if(!naLista){
+                System.out.println("Pokémon não encontrado!");
+            }
+        } else {
+            switch (escolha) {
+                case "v":
+                case "V":
+                    this.trocarTela(new TelaMenuPrincipal(this.contexto));
+                    break;
+                case "x":
+                case "X":
+                    this.contexto.sairPrograma();
+                    break;
+                default:
+                    System.out.println("Por favor insira um valor válido");
+                    break;
+            }
         }
     }
 
