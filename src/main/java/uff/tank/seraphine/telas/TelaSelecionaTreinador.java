f7dd15df976345afe222667df2b0310463181a06
CaelG <cgoncalves@id.uff.br>
2022-12-10 16:16:37 -0300

    Seleção do treinador funcional

--- src/main/java/uff/tank/seraphine/telas/TelaSelecionaTreinador.java
+++ src/main/java/uff/tank/seraphine/telas/TelaSelecionaTreinador.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import uff.tank.seraphine.Treinador;
+import uff.tank.seraphine.utils.JSONUtils;
 
 public class TelaSelecionaTreinador extends Tela {
     @Override
@@ -14,46 +16,72 @@ public class TelaSelecionaTreinador extends Tela {
         JSONParser parser = new JSONParser();
         JSONObject obj = new JSONObject();
 
+        System.out.println("---------- Selecionar Treinador ----------\n");
+        System.out.println("Lista de Treinadores (pelo ID): ");
+
+
         try {
-
-            System.out.println("---------- Selecionar Treinador ----------\n");
-            System.out.println("Lista de Treinadores (pelo ID): ");
-
-            objArray = (JSONArray) parser.parse(new FileReader("assets/dados"));
+            objArray = (JSONArray) parser.parse(new FileReader("assets/dados.json"));
 
             for (Object i : objArray) {
                 obj = (JSONObject) i;
-                System.out.println(obj.toString());
+                //System.out.println(obj.toString());
+                System.out.println(
+                        obj.get("Id").toString() + " - " + obj.get("nome").toString()
+                );
             }
 
             System.out.println("\nV - Voltar ao menu principal");
             System.out.println("X - Sair");
 
+            System.out.print(">");
+
         } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-3);
             // TODO: handle exception
         }
 
         String escolha = this.contexto.getUserInput();
+        boolean isNumber = false;
         String idEscolhido;
 
         if (escolha != "x" && escolha != "v") {
             idEscolhido = escolha;
         }
-        switch (escolha) {
-            case "A":
-                this.trocarTela(new TelaMenuPrincipal(this.contexto));
-                break;
-            case "v":
-            case "V":
-                this.trocarTela(new TelaInicial(this.contexto));
-                break;
-            case "x":
-            case "X":
-                this.contexto.sairPrograma();
-                break;
-            default:
-                System.out.println("Por favor insira um valor válido");
-                break;
+
+        try {
+            //Tenta ver se o input é um número que pode ser Id
+            //Se o parse falhar, a string contém letras, logo, não é um ID
+            Integer.parseInt(escolha);
+            isNumber = true;
+        } catch (NumberFormatException e){
+            isNumber = false;
+        }
+        if(isNumber){
+            this.contexto.setTreinador(
+                    Treinador.getTreinadorFromJSONObject(
+                            JSONUtils.getObjectByID(Integer.parseInt(escolha), "assets/dados.json")
+                    )
+            );
+            this.trocarTela(new TelaMenuPrincipal(this.contexto));
+        } else{
+            switch (escolha) {
+                case "A":
+                    this.trocarTela(new TelaMenuPrincipal(this.contexto));
+                    break;
+                case "v":
+                case "V":
+                    this.trocarTela(new TelaInicial(this.contexto));
+                    break;
+                case "x":
+                case "X":
+                    this.contexto.sairPrograma();
+                    break;
+                default:
+                    System.out.println("Por favor insira um valor válido");
+                    break;
+            }
         }
     }
 
