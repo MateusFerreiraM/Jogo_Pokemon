c0e7567de451118050e5e6010e492c75ac61683c
marvvs <mariacto@id.uff.br>
2022-12-11 00:22:33 -0300

    Ajustes classe Movimentos

--- src/main/java/uff/tank/seraphine/Movimentos.java
+++ src/main/java/uff/tank/seraphine/Movimentos.java
@@ -9,17 +9,29 @@ public class Movimentos extends Identificacao {
     }
 
     private static int movesCriados = 0; // O id de um movimento se baseia em quando ele foi criado
+    private Tipos tipo;
     public int forca;
     public Categoria categoria;
-    public int precisao;
 
     public Movimentos(Tipos tipo, int forca, Categoria categoria, int precisao) {
 
         this.id = movesCriados;
         this.forca = forca;
         this.categoria = categoria;
-        this.precisao = precisao;
 
         movesCriados++;
     }
+
+    public int getForca() {
+        return forca;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public Tipos getTipo() {
+        return tipo;
+    }
+
 }
