88ba43d6859648deb77be149c8fef15b6f87c667
CaelG <cgoncalves@id.uff.br>
2022-12-11 14:40:05 -0300

    Cria metodo para instanciar o Lider de ginasio a partir de um arquivo JSON. Ajusta o lidereres.json

--- src/main/java/uff/tank/seraphine/LiderGin.java
+++ src/main/java/uff/tank/seraphine/LiderGin.java
@@ -1,12 +1,21 @@
 package uff.tank.seraphine;
 
+import org.json.simple.JSONObject;
+import uff.tank.seraphine.utils.JSONUtils;
+
+import java.util.ArrayList;
+
 public class LiderGin extends Treinador {// Lider de Ginásio é um tipo especial de treinador
 
     private Tipos especialidade;// Tipo de pokemon q o lider foca
 
-    public LiderGin(String nome, String regiao, Tipos especialidade) {
+    public LiderGin(String nome, String regiao, int id, Pokemon pkmn) {
         super(nome, regiao);
-        this.especialidade = especialidade;
+        this.id = id;
+        this.pokemonAtual = pkmn;
+        this.pokemons = new ArrayList<Pokemon>();
+        this.pokemons.add(pkmn);
+        this.especialidade = pkmn.tipos.get(0);
     }
 
     public Tipos getEspecialidade() {
@@ -17,10 +26,13 @@ public class LiderGin extends Treinador {// Lider de Ginásio é um tipo especia
         this.especialidade = especialidade;
     }
 
-    /*
-     * TODO: Apenas pokémon com o tipo da especialidade do lider podem
-     * ser parte de seu time.
-     */
-
-    /* TODO: Insignia de ginásio */
+    public static LiderGin getLiderFromJSONObject(JSONObject obj){
+        int id = Integer.parseInt(obj.get("Id").toString());
+        String nome = obj.get("Nome").toString();
+        String regiao = obj.get("Regiao").toString();
+        Pokemon pokemon = Pokemon.getPokemonFromJSONObject(
+                JSONUtils.getObjectByName(obj.get("Pokemon").toString(), "assets/pokemon.json")
+        );
+        return new LiderGin(nome, regiao, id, pokemon);
+    }
 }
