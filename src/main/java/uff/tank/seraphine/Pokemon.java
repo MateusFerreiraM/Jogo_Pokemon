29d18f4448d6ca3e5d999d389a17e6d410386e4b
marvvs <mariacto@id.uff.br>
2022-12-11 00:24:21 -0300

    Add métodos relacionados a batalha
    Add método relacionado a fraqueza/vantagens do pokemon, matriz com essa relação que retorna um multiplicador que será usado na batalha

--- src/main/java/uff/tank/seraphine/Pokemon.java
+++ src/main/java/uff/tank/seraphine/Pokemon.java
@@ -11,13 +11,12 @@ public class Pokemon extends Identificacao {
 
     public static final int NIVEL_EVOLUIR = 50;
     private List<Movimentos> movimentosList;
-    private boolean statusVivo = true;
     private String regiao;
     private int nivel;
     private int hp;
+    private int hpAtual;
     private int ataque;
     private int defesa;
-
     private boolean lendario;
 
     public Pokemon(String nome, List<Tipos> tipos, List<Movimentos> lista) {
@@ -39,8 +38,15 @@ public class Pokemon extends Identificacao {
         return movimentosList;
     }
 
-    public boolean getStatusVivo() {
-        return statusVivo;
+    public String getMovimento(int i){
+        return movimentosList.get(i).getCategoria().toString();
+    }
+
+    public boolean estaVivo() {
+        if (hpAtual > 0) {
+            return true;
+        }
+        return false;
     }
 
     public String getRegiao() {
@@ -55,10 +61,92 @@ public class Pokemon extends Identificacao {
         return hp;
     }
 
+    public int getHpAtual() {
+        return hpAtual;
+    }
+
+    public void setHpAtual(int hpAtual) {
+        this.hpAtual = hpAtual;
+    }
+
+    public void perdeHp (double dano){
+        int danoX = (int) dano;
+        if (this.estaVivo()) {
+            this.hpAtual -= danoX;
+        }
+    }
+
     public int getAtaque(){return ataque;}
 
     public int getDefesa() {
         return defesa;
+    }
+
+    public int associaTipo(Tipos tipo) {
+        if (tipo == Tipos.NORMAL)
+            return 0;
+        else if (tipo == Tipos.LUTADOR)
+            return 1;
+        else if (tipo == Tipos.VOADOR)
+            return 2;
+        else if (tipo == Tipos.VENENO)
+            return 3;
+        else if (tipo == Tipos.TERRA)
+            return 4;
+        else if (tipo == Tipos.PEDRA)
+            return 5;
+        else if (tipo == Tipos.INSETO)
+            return 6;
+        else if (tipo == Tipos.FANTASMA)
+            return 7;
+        else if (tipo == Tipos.ACO)
+            return 8;
+        else if (tipo == Tipos.FOGO)
+            return 9;
+        else if (tipo == Tipos.AGUA)
+            return 10;
+        else if (tipo == Tipos.GRAMA)
+            return 11;
+        else if (tipo == Tipos.ELETRICO)
+            return 12;
+        else if (tipo == Tipos.PEDRA)
+            return 13;
+        else if (tipo == Tipos.GELO)
+            return 14;
+        else if (tipo == Tipos.DRAGAO)
+            return 15;
+        else if (tipo == Tipos.SOMBRIO)
+            return 16;
+        else if (tipo == Tipos.FADA)
+            return 17;
+        else
+            return -1;
+    }
+
+    public double vantagem(Pokemon alvo, Movimentos ataque) {
+        double m = 0.5; // Constante "1/2"
+        double[][] vantagem = { { 1, 1, 1, 1, 1, m, 1, 0, m, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 2, 1, m, m, 1, 2, m, 0, 2, 1, 1, 1, 1, m, 2, 1, 2, m },
+                { 1, 2, 1, 1, 1, m, 2, 1, m, 1, 1, 2, m, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, m, m, m, 1, m, 0, 1, 1, 2, 1, 1, 1, 1, 1, 2 },
+                { 1, 1, 0, 2, 1, 2, m, 1, 2, 2, 1, m, 2, 1, 1, 1, 1, 1 },
+                { 1, m, 2, 1, m, 1, 2, 1, m, 2, 1, 1, 1, 1, 2, 1, 1, 1 },
+                { 1, m, m, m, 1, 1, 1, m, m, m, 1, 2, 1, 2, 1, 1, 2, m },
+                { 0, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, m, 1 },
+                { 1, 1, 1, 1, 1, 2, 1, 1, m, m, m, 1, m, 1, 2, 1, 1, 2 },
+                { 1, 1, 1, 1, 1, m, 2, 1, 2, m, m, 2, 1, 1, 2, m, 1, 1 },
+                { 1, 1, 1, 1, 2, 2, 1, 1, 1, 2, m, m, 1, 1, 1, m, 1, 1 },
+                { 1, 1, m, m, 2, 2, m, 1, m, m, 2, m, 1, 1, 1, m, 1, 1 },
+                { 1, 1, 2, 1, 0, 1, 1, 1, 1, 1, 2, m, m, 1, 1, m, 1, 1 },
+                { 1, 2, 1, 2, 1, 1, 1, 1, m, 1, 1, 1, 1, m, 1, 1, 0, 1 },
+                { 1, 1, 2, 1, 2, 1, 1, 1, m, m, m, 2, 1, 1, m, 2, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, m, 1, 1, 1, 1, 1, 1, 2, 1, 0 },
+                { 1, m, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, m, m },
+                { 1, 2, 1, m, 1, 1, 1, 1, m, m, 1, 1, 1, 1, 1, 2, 2, 1 }
+        };
+        int pkm1 = associaTipo(ataque.getTipo()), pkm2 = associaTipo(alvo.getTipos().get(0));
+        return vantagem[pkm1][pkm2];
+
     }
 
     public static Pokemon getPokemonFromJSONObject(JSONObject obj){
