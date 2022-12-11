7930fbe44d31d908d38cc23e2bf5d91821b8506c
marvvs <mariacto@id.uff.br>
2022-12-11 14:26:37 -0300

    Atualização da classe Batalha e da Tela
    C:\Users\maria\Documents\GitHub\trabalho-poo

--- src/main/java/uff/tank/seraphine/telas/TelaBatalha.java
+++ src/main/java/uff/tank/seraphine/telas/TelaBatalha.java
@@ -1,7 +1,12 @@
 package uff.tank.seraphine.telas;
 
+import java.util.Random;
+
 import org.json.simple.JSONObject;
+
+import uff.tank.seraphine.Batalha;
 import uff.tank.seraphine.CadastroTreinador;
+import uff.tank.seraphine.LiderGin;
 import uff.tank.seraphine.Pokemon;
 import uff.tank.seraphine.Treinador;
 import uff.tank.seraphine.utils.JSONUtils;
@@ -10,59 +15,52 @@ import uff.tank.seraphine.utils.JSONUtils;
 public class TelaBatalha extends Tela {
     @Override
     public void mostrarTela() {
-        int i = this.contexto.getTreinador().getId();
-        int pkmSelecionado = 0;
+        Treinador treinador = this.contexto.getTreinador();
+        LiderGin lider = new LiderGin(null, null, null);
 
-        JSONObject objTreinador = JSONUtils.getObjectByID(i, "assets/dados.json");
-        JSONObject objPkmTreinador = JSONUtils.getObjectByID(pkmSelecionado, "assets/pokemon.json");
+        JSONObject objPkmTreinador = JSONUtils.getObjectByName(treinador.pokemonAtual.getNome(), "assets/pokemon.json");
         JSONObject objLider = JSONUtils.getObjectByID(i, "assets/lideres.json");
 
         int hpTreinador = this.contexto.getTreinador().getPokemons().get(pkmSelecionado).getHpAtual();
         int hpLider = 100;
-        int contagemEspecial = 5;
+        int contEspecial = 2, contEspecialLider = 2;
+
+        Batalha batalha = new Batalha(treinador.pokemonAtual, null);
+
         // TODO: Sistema de batalha
 
         System.out.println("---------- Batalha ----------");
         System.out.println("Seu pokemon: " + objPkmTreinador.get("Nome"));
         System.out.println(
-                "Seu HP atual: " + this.contexto.getTreinador().getPokemons().get(pkmSelecionado).getHpAtual());
+                "Seu HP atual: " + objPkmTreinador.get("HP"));
         System.out.println("Seu ataque: " + objPkmTreinador.get("Ataque"));
         System.out.println("Sua defesa: " + objPkmTreinador.get("Defesa"));
         System.out.println("-----------------------------");
-        System.out.println("Pokemon inimigo: " + obj.get("Nome")); // atribuir dados do pokemon inimigo a variavel e
-                                                                   // buscar ela aqui
+        System.out.println("Pokemon inimigo: " + obj.get("Nome"));
         System.out.println("HP atual do inimigo: " + obj.get("HP"));
         System.out.println("Ataque do inimigo: " + obj.get("Ataque"));
         System.out.println("Defesa do inimigo: " + obj.get("Defesa"));
         System.out.println("-----------------------------\n");
         System.out.println("Escolha seu ataque");
         System.out.println("(1) - Físico");
-        System.out.println("(2) - Especial");
+        System.out.println("(2) - Especial (Restantes: " + contEspecial);
         System.out.println("\nX - Sair");
 
         String escolha = this.contexto.getUserInput();
-        /*
-         * 
-         * static void imprimeHP(int hpUsuario, int hpComputador, int contagemEspeciais)
-         * {
-         * System.out.println("====================");
-         * System.out.println("- HP Usuario: " + hpUsuario);
-         * System.out.println("- HP Computador: " + hpComputador);
-         * System.out.println("* Contagem Especiais: " + contagemEspeciais);
-         * System.out.println("====================");
-         * }
-         */
+
+        Random random = new Random();
+        String randomLider = Integer.toString(random.nextInt(2)+1);
 
         while (hpTreinador > 0 && hpLider > 0) {
             switch (escolha) {
                 case "1":
-                    System.out.println("Usuario aplicou um soco.");
-                    hpLider -= 7;
+                    System.out.println("Usuario aplicou um ataque físico.");
+                    batalha.atacar(treinador.pokemonAtual.getMovimento(0));
                     break;
                 case "2":
                     System.out.println("Usuario aplicou um ataque especial.");
-                    hpLider -= 20;
-                    contagemEspecial--; // contagemespecial = contagemEspecial -1
+                    batalha.atacar(treinador.pokemonAtual.getMovimento(1));
+                    contEspecial--;
                     break;
                 case "x":
                 case "X":
@@ -73,41 +71,30 @@ public class TelaBatalha extends Tela {
                     break;
             }
             if (hpLider > 0) {
-                switch (escolha) {
+                switch (randomLider) {
                     case "1":
-                        System.out.println("Computador aplicou um soco.");
-                        hpTreinador -= 2 + (int) (i / 10);
+                        System.out.println("Líder " + lider.getNome() + " aplicou um ataque físico.");
+                        batalha.atacar(lider.pokemonAtual.getMovimento(0));
                         break;
                     case "2":
-                        System.out.println("Computador aplicou um chute.");
-                        hpTreinador -= 3 + (int) (i / 10);
-                        contagemEspecial--; // contagemespecial = contagemEspecial -1
-                        break;
-                    case "x":
-                    case "X":
-                        this.contexto.sairPrograma();
-                        break;
-                    default:
-                        System.out.println("Por favor insira um valor válido");
+                        if (contEspecialLider>0) {
+                            System.out.println("Líder " + lider.getNome() + " aplicou um ataque especial.");
+                            batalha.atacar(lider.pokemonAtual.getMovimento(1));
+                            contEspecialLider--;
+                        } else {
+                            System.out.println("Líder " + lider.getNome() + " aplicou um ataque físico.");
+                            batalha.atacar(lider.pokemonAtual.getMovimento(0));
+                            break;
+                        }
                         break;
                 }
             } else {
-                System.out.println("Inimigo derrotado");
+                System.out.println("Inimigo derrotado!");
             }
         }
-        if (hpTreinador > 0) {
-            hpTreinador += 5;
-            if (hpTreinador > 150) {
-                hpTreinador = 150;
-            }
-            if (i % 10 == 0) {
-                contagemEspecial++;
-                if (contagemEspecial > 5) {
-                    contagemEspecial = 5;
-                }
-            }
-        }
-        i++;
+        if (hpTreinador <= 0) {
+            System.out.println("Tente novamente depois :(");
+        } 
 
     }
 
