938851607e79b5d664a3ca65596664f9ce40116e
marvvs <mariacto@id.uff.br>
2022-12-16 03:59:45 -0300

    Colocar o calculo da vantagem na classe batalha

--- src/main/java/uff/tank/seraphine/Batalha.java
+++ src/main/java/uff/tank/seraphine/Batalha.java
@@ -37,12 +37,79 @@ public class Batalha {
 
     }
 
+    public int associaTipo(Tipos tipo) {
+        if (tipo == Tipos.NORMAL)
+            return 0;
+        else if (tipo == Tipos.LUTADOR)
+            return 1;
+        else if (tipo == Tipos.VOADOR)
+            return 2;
+        else if (tipo == Tipos.VENENO)
+            return 3;
+        else if (tipo == Tipos.TERRA)
+            return 4;
+        else if (tipo == Tipos.PEDRA)
+            return 5;
+        else if (tipo == Tipos.INSETO)
+            return 6;
+        else if (tipo == Tipos.FANTASMA)
+            return 7;
+        else if (tipo == Tipos.ACO)
+            return 8;
+        else if (tipo == Tipos.FOGO)
+            return 9;
+        else if (tipo == Tipos.AGUA)
+            return 10;
+        else if (tipo == Tipos.GRAMA)
+            return 11;
+        else if (tipo == Tipos.ELETRICO)
+            return 12;
+        else if (tipo == Tipos.PSIQUICO)
+            return 13;
+        else if (tipo == Tipos.GELO)
+            return 14;
+        else if (tipo == Tipos.DRAGAO)
+            return 15;
+        else if (tipo == Tipos.SOMBRIO)
+            return 16;
+        else if (tipo == Tipos.FADA)
+            return 17;
+        else
+            return 0;
+    }
+
+    public double vantagem(Pokemon alvo, Movimentos ataque) {
+        double m = 0.5; // Constante "1/2"
+        double[][] vantagem = { { 1, 1, 1, 1, 1, m, 1, 0, m, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 2, 1, m, m, 1, 2, m, 0, 2, 1, 1, 1, 1, m, 2, 1, 2, m },
+                { 1, 2, 1, 1, 1, m, 2, 1, m, 1, 1, 2, m, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, m, m, m, 1, m, 0, 1, 1, 2, 1, 1, 1, 1, 1, 2 },
+                { 1, 1, 0, 2, 1, 2, m, 1, 2, 2, 1, m, 2, 1, 1, 1, 1, 1 },
+                { 1, m, 2, 1, m, 1, 2, 1, m, 2, 1, 1, 1, 1, 2, 1, 1, 1 },
+                { 1, m, m, m, 1, 1, 1, m, m, m, 1, 2, 1, 2, 1, 1, 2, m },
+                { 0, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, m, 1 },
+                { 1, 1, 1, 1, 1, 2, 1, 1, m, m, m, 1, m, 1, 2, 1, 1, 2 },
+                { 1, 1, 1, 1, 1, m, 2, 1, 2, m, m, 2, 1, 1, 2, m, 1, 1 },
+                { 1, 1, 1, 1, 2, 2, 1, 1, 1, 2, m, m, 1, 1, 1, m, 1, 1 },
+                { 1, 1, m, m, 2, 2, m, 1, m, m, 2, m, 1, 1, 1, m, 1, 1 },
+                { 1, 1, 2, 1, 0, 1, 1, 1, 1, 1, 2, m, m, 1, 1, m, 1, 1 },
+                { 1, 2, 1, 2, 1, 1, 1, 1, m, 1, 1, 1, 1, m, 1, 1, 0, 1 },
+                { 1, 1, 2, 1, 2, 1, 1, 1, m, m, m, 2, 1, 1, m, 2, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, m, 1, 1, 1, 1, 1, 1, 2, 1, 0 },
+                { 1, m, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, m, m },
+                { 1, 2, 1, m, 1, 1, 1, 1, m, m, 1, 1, 1, 1, 1, 2, 2, 1 }
+        };
+        int atk = associaTipo(ataque.getTipo()), dsf = associaTipo(alvo.getTipos().get(0));
+        return vantagem[atk][dsf];
+
+    }
+
     public int calculoVantagem(Pokemon quemAtk, Pokemon alvo, Movimentos atk) {
         int dano = 0;
         if (atk.categoria == Categoria.FISICO) {
-            dano = (int) (quemAtk.vantagem(alvo, atk) * quemAtk.getAtaque() * atk.forca) - alvo.getDefesa() / 2;
+            dano = (int) (vantagem(alvo, atk) * quemAtk.getAtaque() * atk.forca) - alvo.getDefesa() / 2;
         } else {
-            dano = (int) (quemAtk.vantagem(alvo, atk) * quemAtk.getAtaque() * atk.forca) - alvo.getDefesa() / 2;
+            dano = (int) (vantagem(alvo, atk) * quemAtk.getAtaque() * atk.forca) - alvo.getDefesa() / 2;
         }
         if (dano <= 0) {
             return 5;
